5cd95c5a2ba3a5d8f5b1b10d3e059948ca95d049
PierreEdFils <epierrefils@gmail.com>
2022-10-16 10:01:38 -0400

    Initial commit

---
+++ src/com/careerdevs/WelcomeToJava.java
@@ -0,0 +1,10 @@
+package com.careerdevs;
+
+public class WelcomeToJava {
+
+    public static void helloJava () {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+
+}
