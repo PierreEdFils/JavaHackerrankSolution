875209f3e0b534b1973b14551d692b66cc656dbe
PierreEdFils <93496035+PierreEdFils@users.noreply.github.com>
2022-12-12 23:18:58 -0500

    Java Into String

---
+++ src/com/careerdevs/JavaIntoString.java
@@ -0,0 +1,23 @@
+package com.careerdevs;
+
+import java.util.Scanner;
+
+public class JavaIntoString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        n = 0 ;
+
+
+
+        if( n>=-100 && n<=100)
+        {
+            Integer.toString(n);
+            System.out.println("Good job");
+        }
+        else
+        {
+            System.out.println("Wrong answer");
+        }
+    }
+}
---
+++ src/com/careerdevs/JavaLoopsI.java
@@ -0,0 +1,11 @@
+package com.careerdevs;
+
+public class JavaLoopsI {
+    public static void main(String[] args) {
+        int N = 2;
+        for(int i=1; i<=10; i++) {
+            System.out.println(N + " x " + i + " = " + N*i);
+        }
+
+    }
+}
