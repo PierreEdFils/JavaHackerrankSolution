6ff34b90d112dc71e1c02bae861e61e475e3be71
Pierre Fils <epierrefils@gmail.com>
2023-03-03 06:17:10 -0500

    Java Biset Java Stack  Hackerrank final Solution

---
+++ src/com/careerdevs/JavaStack.java
@@ -0,0 +1,36 @@
+package com.careerdevs;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class JavaStack {
+    public static void main(String []argh)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String input=sc.next();
+            //Complete the code
+            System.out.println(isBalanced(input));
+
+        }
+
+    }
+    private static boolean isBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
