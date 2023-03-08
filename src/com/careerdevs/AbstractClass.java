1cdbd3a1a3295ad0b5a6037be1358e5c0fc992db
Pierre Fils <epierrefils@gmail.com>
2023-03-07 23:21:25 -0500

    Java Abstract Class  final Solution

---
+++ src/com/careerdevs/AbstractClass.java
@@ -0,0 +1,37 @@
+package com.careerdevs;
+import java.util.*;
+abstract class Book{
+    String title;
+    abstract void setTitle(String s);
+    String getTitle(){
+        return title;
+    }
+
+}
+
+//Write MyBook class here
+
+class MyBook extends  Book{
+
+    @Override
+    void setTitle(String s) {
+        this.title = s;
+    }
+    String titlefinal(String s)
+    {
+        setTitle(s);
+        return super.title;
+    }
+}
+public class AbstractClass {
+    public static void main(String []args){
+        //Book new_novel=new Book(); This line prHMain.java:25: error: Book is abstract; cannot be instantiated
+        Scanner sc=new Scanner(System.in);
+        String title=sc.nextLine();
+        MyBook new_novel=new MyBook();
+        new_novel.setTitle(title);
+        System.out.println("The title is: "+new_novel.getTitle());
+        sc.close();
+
+    }
+}
