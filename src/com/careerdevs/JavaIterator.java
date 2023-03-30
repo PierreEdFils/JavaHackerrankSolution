18be31d70907e3257015e14d822889ed11056a2d
Pierre Fils <epierrefils@gmail.com>
2023-03-29 23:59:20 -0400

    JAva Iterator  hackerrank solution

---
+++ src/com/careerdevs/JavaIterator.java
@@ -0,0 +1,28 @@
+package com.careerdevs;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class JavaIterator {
+    public static void main(String []args){
+        ArrayList mylist = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for(int i = 0;i<n;i++){
+            mylist.add(sc.nextInt());
+        }
+
+        mylist.add("###");
+        for(int i=0;i<m;i++){
+            mylist.add(sc.next());
+        }
+
+        Iterator it=func(mylist);
+        while(it.hasNext()){
+            Object element = it.next();
+            System.out.println((String)element);
+        }
+    }
+}
